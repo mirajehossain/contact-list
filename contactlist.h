3cf1073bce5ac08a94796c0e53ef7d46f275c6d0
mirajehossain <mdalamin8521@gmail.com>
2018-04-25 00:25:01 +0600

    declare additional header functions

--- contactlist.h
+++ contactlist.h
@@ -6,5 +6,14 @@
     /// main functions prototypes
 
     void addNewContact();
+    void updateContact();
+    void updateContact();
+    void searchByFirstName();
+    void searchByLastName();
+    void searchByNumber();
+    void searchByEmail();
+    void getAllContact();
+    void deleteContact();
+    void saveContact();
 
 #endif // CONTACTLIST_H
